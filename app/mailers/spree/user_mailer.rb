2634c1556bef807e00a8e86e9a948e838fd06290
zhang ze <david@david.home>
2012-09-16 00:39:44 +0800

    preview mail only Rails.env !~ /production/

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -4,13 +4,15 @@ class Spree::UserMailer < ActionMailer::Base
     mail(:to => user.email,
          :subject => user.site[:name] )
   end
-  class Preview < MailView
-    def load_sample()
-      site = Spree::Site.create!(:name=>"ABC",:domain=>"www.example.com")
-      user = site.users.create!(:email=>'abc@example.com',:password=>"spree123",:password_confirmation=>"spree123")
-      mail = Spree::UserMailer.load_sample(user)
-      site.destroy
-      mail
+  if Rails.env !~ /production/
+    class Preview < MailView
+      def load_sample()
+        site = Spree::Site.create!(:name=>"ABC",:domain=>"www.example.com")
+        user = site.users.create!(:email=>'abc@example.com',:password=>"spree123",:password_confirmation=>"spree123")
+        mail = Spree::UserMailer.load_sample(user)
+        site.destroy
+        mail
+      end
     end
   end
 end
