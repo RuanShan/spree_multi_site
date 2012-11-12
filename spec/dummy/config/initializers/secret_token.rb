4bd8218568f70c32607d51745d212fcce407aa9b
zhang ze <david@david.home>
2012-11-12 23:10:53 +0800

    remove spec/dummy .project

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'bbbcc8799b6e156d7fd4c5ad0d80b0d637ce34e93c13a3e02e3e454f34e2a888b2bcc963ccfb6fc49a9f74b7da5debf0d2c07501f8d84040ed69ca6ed59c271b'
