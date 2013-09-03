5dfb75bc2e7bea7bc7234d6b784c29044a9782b3
david <areq22@gmail.com>
2013-09-03 23:29:07 +0800

    fix db migration for 2-0-stable.

--- db/migrate/20121216093739_last_fix_unique_index.rb
+++ db/migrate/20121216093739_last_fix_unique_index.rb
@@ -2,12 +2,11 @@ class LastFixUniqueIndex < ActiveRecord::Migration
   def change
     if Spree::Site.table_exists?
       
-      remove_index "spree_users", :name => "email_idx_unique"
-      add_index "spree_users", ["site","email"], :name => "email_idx_unique", :unique => true
+      add_index "spree_users", ["site_id","email"], :name => "email_idx_unique", :unique => true
       
       
       remove_index :spree_preferences, :name => 'index_spree_preferences_on_key'
-      add_index "spree_preferences", ["site","key"], :name => "index_spree_preferences_on_key", :unique => true
+      add_index "spree_preferences", ["site_id","key"], :name => "index_spree_preferences_on_key", :unique => true
 
     end
   end
