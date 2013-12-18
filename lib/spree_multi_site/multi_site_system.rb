8b0123f025f6ffb98f752f6b178fd84cd0706ec4
david <areq22@gmail.com>
2013-12-18 23:52:18 +0800

    add some comments

--- lib/spree_multi_site/multi_site_system.rb
+++ lib/spree_multi_site/multi_site_system.rb
@@ -43,8 +43,10 @@ module Spree
         site = Spree::Site.find_by_domain(request.host) 
       end
       
-      if Rails.env !~ /prduction/ && site.blank?  
+      if(( Rails.env !~ /prduction/ ) && ( site.blank? ) )  
         # for development or test, enable get site from cookie
+        #Rails.logger.debug "request.cookie_jar=#{request.cookie_jar.inspect},#{request.cookie_jar[:abc_development_domain]},#{request.cookie_jar['abc_development_domain']}"
+        #string and symbol both OK.  cookie.domain should be exactly same as host, www.domain.com != domain.com
         if request.cookie_jar[:abc_development_domain].present?
           site = Spree::Site.find_by_domain( request.cookie_jar[:abc_development_domain] )
         elsif request.cookie_jar[:abc_development_short_name].present?
