35109d0af082f371da253ba45dbf5042a7b5a3d7
david <areq22@gmail.com>
2014-01-02 20:58:16 +0800

    change image path from spree to shops shops is shared folder while deploy

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Image.class_eval do
 #override attachement[:path]      
-  Spree::Image.attachment_definitions[:attachment][:url] = '/spree/:site/products/:id/:basename_:style.:extension'
+  Spree::Image.attachment_definitions[:attachment][:url] = '/shops/:site/products/:id/:basename_:style.:extension'
 
-  Spree::Image.attachment_definitions[:attachment][:path] = ':rails_root/public/spree/:site/products/:id/:basename_:style.:extension'
+  Spree::Image.attachment_definitions[:attachment][:path] = ':rails_root/public/shops/:site/products/:id/:basename_:style.:extension'
 
 end
